197c5b31f322af29d66b03045563f2582a4aa519
krisi01 <chrisisbeast@abv.bg>
2020-12-07 08:06:22 -0800

    Exercise

---
+++ AssociativeArrays/Movies.js
@@ -0,0 +1,32 @@
+function solve(commands) {
+    const movies = {};
+  
+    commands.forEach((command) => {
+      if (command.slice(0, 9) === "addMovie ") {
+        const name = command.slice(9);
+        movies[name]={name:name};
+      }
+  
+      if (command.indexOf(" directedBy ") > -1) {
+        const [name, director] = command.split(" directedBy ");
+        if (movies[name]) { 
+          // This will only be true if an 'addMovie' has added this key to 'movies'.
+          movies[name].director = director;
+        }
+      }
+  
+      if (command.indexOf(" onDate ") > -1) {
+        const [name, date] = command.split(" onDate ");
+        if (movies[name]) {
+          movies[name].date = date;
+        }
+      }
+    });
+  
+    Object.keys(movies).forEach(name=>{
+      const movie=movies[name];
+      if(movie && movie.director && movie.date){
+        console.log(JSON.stringify(movie))
+      }
+    })
+  }
