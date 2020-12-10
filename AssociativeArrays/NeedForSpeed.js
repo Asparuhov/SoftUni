259a0dacd6594092f62e46257e2ca05dde80efab
krisi01 <chrisisbeast@abv.bg>
2020-12-10 05:13:38 -0800

    Finished exercise

--- AssociativeArrays/NeedForSpeed.js
+++ AssociativeArrays/NeedForSpeed.js
@@ -1,6 +1,5 @@
 function solve(input) {
     let num = Number(input.shift());
-    let line = input.shift();
     let cars = {};
     let commands = {
         Drive(model, run, fuel) {
@@ -10,26 +9,68 @@ function solve(input) {
             else if (fuel <= cars[model][1]) {
                 cars[model][1] -= fuel;
                 cars[model][0] += run;
-                console.log(`"${model} driven for ${run} kilometers. ${fuel} liters of fuel consumed`);
+                console.log(`${model} driven for ${run} kilometers. ${fuel} liters of fuel consumed.`);
             }
             if (cars[model][0] >= 100000) {
                 delete cars[model];
                 console.log(`Time to sell the ${model}!`);
-            },
-            
+            }
+        },
+        Refuel(model, fuel) {
+            let startingFuel = cars[model][1];
+            cars[model][1] += fuel;
+            if (cars[model][1] >= 75) {
+                cars[model][1] = 75;
+            }
+            console.log(`${model} refueled with ${cars[model][1] - startingFuel} liters`);
+        },
+        Revert(model, distance) {
+            let startingDis = cars[model][0];
+            cars[model][0] -= distance;
+            if (cars[model][0] <= 10000) {
+                cars[model][0] = 10000;
+            }
+            else {console.log(`${model} mileage decreased by ${distance} kilometers`);}
         }
     }
+  
     while (num !== 0) {
+        let line = input.shift();
         let [model, run, fuel] = line.split('|');
         fuel = Number(fuel);
         run = Number(run);
         cars[model] = [run, fuel];
-        line = input.shift();
         num--;
     }
-    console.log(cars);
+    
+    let set;
+    while ((set = input.shift()) !== 'Stop') {
+        if (set.includes('Drive')) {
+            let [_, model, run, fuel] = set.split(' : ');
+            run = Number(run);
+            fuel = Number(fuel);
+            commands.Drive(model, run, fuel);
+        }
+        if (set.includes('Refuel')) {
+            let [_,model, fuel] = set.split(' : ');
+            fuel = Number(fuel);
+            commands.Refuel(model, fuel);
+        }
+        if (set.includes('Revert')) {
+            let [_,model, run] = set.split(' : ');
+            run = Number(run)
+            commands.Revert(model, run);
+        }
+    }
+    const sortable = Object.fromEntries(
+        Object.entries(cars).sort(([,a],[,b]) => b[0] - a[0] )
+    );
+    let result = Object.keys(sortable).sort((a, b) => a.localeCompare(b));
+    Object.keys(sortable).forEach(key => console.log(`${key} -> Mileage: ${cars[key][0]} kms, Fuel in the tank: ${cars[key][1]} lt.`)) 
 }
- /* solve([
+    
+
+ solve([
     '3',
     'Audi A6|38000|62',
     'Mercedes CLS|11000|35',
@@ -42,21 +83,5 @@ function solve(input) {
     'Revert : Audi A6 : 30000',
     'Stop'
   ]
-  );  */
+  ); 
 
-
-
-
-
-  
-let cars = {
-    'Audi A6': [ 38000, 62 ],
-    'Mercedes CLS': [ 11000, 35 ],
-    'Volkswagen Passat CC': [ 45678, 5 ]
-  }
-  
-function Refuel(model, fuel) {
-    
-}
-
-Drive(`Audi A6`, 543, 47);
