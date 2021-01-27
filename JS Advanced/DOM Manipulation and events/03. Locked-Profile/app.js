d2b22d821f0bf0e72fd7da4e20601e714a222182
krisi01 <chrisisbeast@abv.bg>
2021-01-27 20:22:09 +0200

    problem solving

--- JS Advanced/DOM Manipulation and events/03. Locked-Profile/app.js
+++ JS Advanced/DOM Manipulation and events/03. Locked-Profile/app.js
@@ -1,3 +1,33 @@
 function lockedProfile() {
-    console.log('TODO...')
+  //User 1
+  let user1 = document.getElementsByClassName("profile")[0];
+    let user1Button = user1.getElementsByTagName("button")[0];
+    console.log(user1);
+    if (user1Button.textContent === 'Show more') {
+        user1Button.addEventListener("click", () => {
+            user1.getElementsByTagName("div")[0].style.display = "inline";
+            user1Button.textContent = "Hide it";
+        });
+    }
+    if (user1Button.textContent === 'Hide it') {
+        console.log('clicked');
+        user1Button.addEventListener("click", () => {
+            user1.getElementsByTagName("div")[0].style.display = "";
+            user1Button.textContent = "Show more";
+        });
+    }
+    console.log(user1Button.textContent);
+  //User 2
+  let user2 = document.getElementsByClassName("profile")[1];
+  let user2Button = user2.getElementsByTagName("button")[0];
+  user2Button.addEventListener("click", () => {
+    user2.getElementsByTagName("div")[0].style.display = "inline";
+  });
+  //User 3
+  let user3 = document.getElementsByClassName("profile")[2];
+    let user3Button = user3.getElementsByTagName("button")[0];
+    let clicked3 = false;
+  user3Button.addEventListener("click", () => {
+    user3.getElementsByTagName("div")[0].style.display = "inline";
+  });
 }
