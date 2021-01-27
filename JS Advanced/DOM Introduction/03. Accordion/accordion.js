76e8b732a1c96145e716deef808a8b28fe1852a3
krisi01 <chrisisbeast@abv.bg>
2021-01-27 19:02:10 +0200

    plain html practice

---
+++ JS Advanced/DOM Introduction/03. Accordion/accordion.js
@@ -0,0 +1,5 @@
+function toggle() {
+    let isClicked = false;
+    document.getElementById('extra').style.display = 'inline';
+    document.getElementsByClassName("button")[0].textContent = 'Less';
+}
