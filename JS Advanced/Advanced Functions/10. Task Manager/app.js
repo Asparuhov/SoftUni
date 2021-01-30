142413e73d3a9ac392b0cbe3a308cdbd6507bd89
krisi01 <chrisisbeast@abv.bg>
2021-01-30 20:31:00 +0200

    fixing problem

--- JS Advanced/Advanced Functions/10. Task Manager/app.js
+++ JS Advanced/Advanced Functions/10. Task Manager/app.js
@@ -7,6 +7,9 @@ function solve() {
     const desc = document.getElementById("description").value;
     const date = document.getElementById("date").value;
     const createArticle = () => {
+      let passedTask = task;
+      let passedDesc = desc;
+      let passedDate = date;
       let newArticle = document.createElement("article");
       //h3 insert
       let h3 = document.createElement("h3");
@@ -15,12 +18,12 @@ function solve() {
       newArticle.appendChild(h3);
       //p insert
       let p = document.createElement("p");
-      let pText = document.createTextNode(desc);
+      let pText = document.createTextNode(`Description: ${desc}`);
       p.appendChild(pText);
       newArticle.appendChild(p);
       //date insert
       let p1 = document.createElement("p");
-      let p1Text = document.createTextNode(date);
+      let p1Text = document.createTextNode(`Due Date: ${date}`);
       p1.appendChild(p1Text);
       newArticle.appendChild(p1);
       //insert div
@@ -29,19 +32,73 @@ function solve() {
       //create and insert button into div
       let button1 = document.createElement("button");
       button1.classList.add("red");
-        button1.textContent = "Delete";
-        button1.addEventListener('click', (e) => {
-            e.target.type = 'button';
-            newArticle.remove();
-      })
+      button1.textContent = "Delete";
+      button1.addEventListener("click", (e) => {
+        e.target.type = "button";
+        newArticle.remove();
+      });
       div.appendChild(button1);
       let button2 = document.createElement("button");
       button2.classList.add("green");
       button2.textContent = "Start";
+      button2.addEventListener("click", (e) => {
+        let task1 = passedTask;
+        let desc1 = passedDesc;
+        let date1 = passedDate;
+        e.target.type = "button";
+        let progress = document.getElementById("in-progress");
+        console.log(progress);
+        let article1 = document.createElement("article");
+        //insert h3
+        let h3 = document.createElement("h3");
+        let h3Text = document.createTextNode(passedTask);
+        h3.appendChild(h3Text);
+        article1.appendChild(h3);
+        //insert p
+        let p = document.createElement("p");
+        let pText = document.createTextNode(`Description: ${passedDesc}`);
+        p.appendChild(pText);
+        article1.appendChild(p);
+        //date insert
+        let p1 = document.createElement("p");
+        let p1Text = document.createTextNode(`Due Date: ${passedDate}`);
+        p1.appendChild(p1Text);
+        article1.appendChild(p1);
+        //insert div
+        let div1 = document.createElement("div");
+        div1.classList.add("flex");
+        //create and insert button into div
+        let button1 = document.createElement("button");
+        button1.classList.add("red");
+        button1.textContent = "Delete";
+        button1.addEventListener("click", (e) => {
+          e.target.type = "button";
+          article1.remove();
+        });
+        div1.appendChild(button1);
+        let button2 = document.createElement("button");
+        button2.classList.add("orange");
+        button2.textContent = "Finish";
+        button2.addEventListener("click", (e) => {
+          e.target.type = "button";
+          let modifiedArticle = article1;
+          let div = modifiedArticle.getElementsByTagName("div")[0];
+          modifiedArticle.removeChild(div);
+          let complete = document.querySelectorAll("section")[3];
+          let div1 = complete.querySelectorAll("div")[1];
+          div1.appendChild(modifiedArticle);
+        });
+        div1.appendChild(button2);
+        article1.appendChild(div1);
+        progress.appendChild(article1);
+        newArticle.remove();
+      });
       div.appendChild(button2);
       newArticle.appendChild(div);
       return newArticle;
     };
-    openDiv.querySelectorAll("div")[1].appendChild(createArticle());
+    if (desc && task && date) {
+      openDiv.querySelectorAll("div")[1].appendChild(createArticle());
+    }
   });
 }
